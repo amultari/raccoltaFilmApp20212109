9c1dd8a74d73653661cd3f91093320adc12fcfb5
a.monterosso87@gmail.com <a.monterosso87@gmail.com>
2022-12-06 17:40:35 +0100

    implementato matirial nella pagina di login

--- src/app/core/auth/login/login.module.ts
+++ src/app/core/auth/login/login.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import {LoginComponent} from "./login.component";
 import {RouterModule, Routes} from "@angular/router";
 import {FormsModule} from "@angular/forms";
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
 
 const routes: Routes = [
   {
@@ -19,6 +23,10 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     RouterModule.forChild(routes),
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule
   ],
   exports: [
     LoginComponent
